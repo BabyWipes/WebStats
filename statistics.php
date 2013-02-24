d69876fff6f5f66f9bebaa25849e75c7ec3bb049
Zach De Koning <zach.de.koning@gmail.com>
2013-02-24 15:43:33 +1100

    New stuff! (More coming!)

--- statistics.php
+++ statistics.php
@@ -1,10 +1,9 @@
-
 <?php
 
 $link = mysql_connect ("host", "user", "password")
                    or die('Could not connect: ' . mysql_error());
 				   
-				   mysql_select_db ("oresomec_battle", $link) or die('could not select database'); 
+				   mysql_select_db ("database", $link) or die('could not select database'); 
 				   
 				   $username = $_GET["name"];
 				   
@@ -12,24 +11,55 @@ $link = mysql_connect ("host", "user", "password")
 					 $fullname_result = mysql_query($fullname)or die('query failed'. mysql_error());
 				   $realname = mysql_fetch_assoc($fullname_result);
                     $final_fullname = $realname['name'];
-				   			if (mysql_num_rows($fullname_result) === 0) {
-								
-							
-    die( "User not found. Please try refreshing this page and searching for a user that exists." );
-	
-	
-	
-	
-				}  else  {
+                    
+		if (mysql_num_rows($fullname_result) === 0) { ?>
+
+
+    <html>
+    
+     <style type="text/css">
+
+  .boxdiv {
+      margin: 5px;
+      padding: 5px;
+      width: 45%;
+      position: fixed;
+      left: 25%;
+      border: 2px solid #4D686F;
+      background-color: #E6E6E6;
+      border-radius: 8px;
+  }
+  </style>
+    
+    
+    <br>
+    <div class="boxdiv">
+    		<form action="statistics.php" method="get">
+			<p style="text-align: center;">
+				<strong><span style="font-size:24px;"><span style="font-family: arial, helvetica, sans-serif;">Sorry, that user wasn't found in the database! Please search for an existing user below:</span></span></strong></p>
+			<p style="text-align: center;">&nbsp;
+				</p>
+			<p style="text-align: center;">
+				<span style="font-family:arial,helvetica,sans-serif;"><strong><span style="font-size: 20px;">Username: </span></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" /></p>
+			<p style="text-align: center;">&nbsp;
+				</p>
+    
+    </div>
+    
+    
+    </html>
+
+				 <? 
+				 die();
+				 } else { 
 				   
 				   $kills = "SELECT kills FROM `oresomebattles` WHERE name='$final_fullname'";
 				   $deaths = "SELECT deaths FROM `oresomebattles` WHERE name='$final_fullname'";
-				  
-
-	
+				   $ffawins = "SELECT ffawins FROM `oresomebattles` WHERE name='$final_fullname'";
+			
 	$kills_result = mysql_query($kills)or die('query failed'. mysql_error());			   
- $deaths_result = mysql_query($deaths)or die('query failed'. mysql_error());
-
+ $deaths_result = mysql_query($deaths)or die('query failed'. mysql_error());		   
+ $ffawins_result = mysql_query($ffawins)or die('query failed'. mysql_error());
  
 $row = mysql_fetch_assoc($kills_result);
 $final_kills = $row['kills'];
@@ -37,22 +67,33 @@ $final_kills = $row['kills'];
 $row = mysql_fetch_assoc($deaths_result);
 $final_deaths = $row['deaths'];
 
-
-				
-
-
-
+$row = mysql_fetch_assoc($ffawins_result);
+$final_ffawins = $row['ffawins'];
 
 mysql_close ($link);
 
- 
-				}
+  }
 				
 	
 ?>
 
 <html>
 
+ <style type="text/css">
+
+  .boxdiv {
+      margin: 5px;
+      padding: 5px;
+      width: 65%;
+      position: fixed;
+      left: 15%;
+      border: 2px solid #4D686F;
+      background-color: #E6E6E6;
+      border-radius: 8px;
+  }
+  </style>
+
+<div class="boxdiv">
 <p style="text-align: center;">
 <em><strong><span style="font-family: arial, helvetica, sans-serif; font-size: 72px; text-align: center;"> <?php echo $final_fullname ?> </span></span></strong></em></p>
 <p style="text-align: center;">
@@ -68,8 +109,10 @@ mysql_close ($link);
 <p style="text-align: center;">
 <strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Kill / Death ratio: </span></span></strong>  <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"><?php 
 
-if ($final_kills / $final_deaths === 0) {
-	echo 0;
+if ($final_kills / $final_deaths === 0 OR $final_kills / $final_deaths < 0 AND $final_deaths === 0) {
+	echo $final_kills;
+
+	
 } else {
 
 $kdr = $final_kills / $final_deaths;
@@ -80,6 +123,12 @@ echo $formatted_kdr;
 				?> 
 
 </span></span></p>
+
+
+<p style="text-align: center;">
+<strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Free for alls won: </span></span></strong> <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> <?php echo $final_ffawins ?> </span></span></p>
+
+
 <form>
 <p style="text-align: center;">
  <input type=button value="Refresh" onClick="window.location.reload()"></form>
@@ -88,4 +137,7 @@ echo $formatted_kdr;
 			
 				</p>
 			<p style="text-align: center;">
-				<span style="font-family:arial,helvetica,sans-serif;"><strong><em><span style="font-size: 24px;">Search another user: </span></em></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" /></p></html>
+				<span style="font-family:arial,helvetica,sans-serif;"><strong><em><span style="font-size: 24px;">Search another user: </span></em></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" /></p>
+				  </div>
+				
+				</html>

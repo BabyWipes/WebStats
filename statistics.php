e33f443b592c0b2a33646b7e68287894df766d30
Zach De Koning <zach.de.koning@gmail.com>
2013-05-22 22:05:16 +1000

    Reformat to make it a bit more readable

--- statistics.php
+++ statistics.php
@@ -1,53 +1,136 @@
 
 <?php
+$link = mysql_connect ("203.33.121.41", "battles_stats", "")
+    or die('Could not connect: ' . mysql_error());
 
-$link = mysql_connect ("Host", "User", "Password")
-                   or die('Could not connect: ' . mysql_error());
-				   
-				   mysql_select_db ("oresomec_battle", $link) or die('could not select database'); 
-				   
-				   $username = $_GET["name"];
-				   
-				   $kills = "SELECT kills FROM `oresomebattles` WHERE name='$username'";
-				   $deaths = "SELECT deaths FROM `oresomebattles` WHERE name='$username'";
-				
-	
-	$kills_result = mysql_query($kills)or die('query failed'. mysql_error());			   
- $deaths_result = mysql_query($deaths)or die('query failed'. mysql_error());
- 
-$row = mysql_fetch_assoc($kills_result);
-$final_kills = $row['kills'];
+mysql_select_db ("battle", $link) or die('could not select database');
 
-$row = mysql_fetch_assoc($deaths_result);
-$final_deaths = $row['deaths'];
+$username = $_GET["name"];
+$fullname = "SELECT name FROM `oresomebattles` WHERE name='$username'";
+$fullname_result = mysql_query($fullname)or die('query failed'. mysql_error());
+$realname = mysql_fetch_assoc($fullname_result);
+$final_fullname = $realname['name'];
 
-///$kd = final_kills /final_deaths;
+if (mysql_num_rows($fullname_result) === 0) { ?>
 
-mysql_close ($link);
+    <html>
+
+    <style type="text/css">
+
+        .boxdiv {
+            margin: 5px;
+            padding: 5px;
+            width: 45%;
+            position: fixed;
+            left: 25%;
+            border: 2px solid #4D686F;
+            background-color: #E6E6E6;
+            border-radius: 8px;
+        }
+    </style>
+
+    <br>
+    <div class="boxdiv">
+        <form action="statistics.php" method="get">
+            <p style="text-align: center;">
+                <strong><span style="font-size:24px;"><span style="font-family: arial, helvetica, sans-serif;">Sorry, that user wasn't found in the database! Please search for an existing user below:</span></span></strong></p>
+            <p style="text-align: center;">&nbsp;
+            </p>
+            <p style="text-align: center;">
+                <span style="font-family:arial,helvetica,sans-serif;"><strong><span style="font-size: 20px;">Username: </span></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" /></p>
+            <p style="text-align: center;">&nbsp;
+            </p>
+
+    </div>
 
 
- 
+    </html>
+
+    <?
+    die();
+} else {
+
+    $kills = "SELECT kills FROM `oresomebattles` WHERE name='$final_fullname'";
+    $deaths = "SELECT deaths FROM `oresomebattles` WHERE name='$final_fullname'";
+    $ffawins = "SELECT ffawins FROM `oresomebattles` WHERE name='$final_fullname'";
+
+    $kills_result = mysql_query($kills)or die('query failed'. mysql_error());
+    $deaths_result = mysql_query($deaths)or die('query failed'. mysql_error());
+    $ffawins_result = mysql_query($ffawins)or die('query failed'. mysql_error());
+
+    $row = mysql_fetch_assoc($kills_result);
+    $final_kills = $row['kills'];
+
+    $row = mysql_fetch_assoc($deaths_result);
+    $final_deaths = $row['deaths'];
+
+    $row = mysql_fetch_assoc($ffawins_result);
+    $final_ffawins = $row['ffawins'];
+
+    mysql_close ($link);
+
+}
+
+
 ?>
 
 <html>
 
-<p style="text-align: center;">
-<em><strong><span style="font-family: arial, helvetica, sans-serif; font-size: 72px; text-align: center;"> <?php echo $username ?> </span></span></strong></em></p>
-<p style="text-align: center;">
-<img src="https://minotar.net/avatar/<?php echo $username?>/200">
+<style type="text/css">
 
-<p style="text-align: center;">
-<strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> Kills: </span></span></strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> <?php echo $final_kills ?> </span></span></p>
+    .boxdiv {
+        margin: 5px;
+        padding: 5px;
+        width: 65%;
+        position: fixed;
+        left: 15%;
+        border: 2px solid #4D686F;
+        background-color: #E6E6E6;
+        border-radius: 8px;
+    }
+</style>
 
-<p style="text-align: center;">
-<strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Deaths: </span></span></strong> <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> <?php echo $final_deaths ?> </span></span></p>
+<div class="boxdiv">
+    <p style="text-align: center;">
+        <em><strong><span style="font-family: arial, helvetica, sans-serif; font-size: 72px; text-align: center;"> <?php echo $final_fullname ?> </span></span></strong></em></p>
+    <p style="text-align: center;">
 
-<p style="text-align: center;">
-<strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Kill / Death ratio: </span></span></strong>  <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"><?php echo $final_kills / $final_deaths?> </span></span></p>
-<br>
-<br>
-<br>
-<p style="text-align: center;">
-	<span style="font-size:48px;"><a href="http://oresomecraft.com/stats.php"><span style="font-family: arial, helvetica, sans-serif;">Home</span></a></span></p>
+        <img src="https://minotar.net/avatar/<?php echo $final_fullname?>/200">
+
+    <p style="text-align: center;">
+        <strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> Kills: </span></span></strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> <?php echo $final_kills ?> </span></span></p>
+
+    <p style="text-align: center;">
+        <strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Deaths: </span></span></strong> <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> <?php echo $final_deaths ?> </span></span></p>
+
+    <p style="text-align: center;">
+        <strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Kill / Death ratio: </span></span></strong>  <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"><?php
+
+                if ($final_kills / $final_deaths === 0 OR $final_kills / $final_deaths < 0 AND $final_deaths === 0) {
+                    echo $final_kills;
+
+                } else {
+
+                    $kdr = $final_kills / $final_deaths;
+                    $formatted_kdr = round($kdr,2);
+
+                    echo $formatted_kdr;
+                }
+                ?>
+
+</span></span></p>
+
+    <p style="text-align: center;">
+        <strong><span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;">Free for alls won: </span></span></strong> <span style="font-size:36px;"><span style="font-family: arial, helvetica, sans-serif;"> <?php echo $final_ffawins ?> </span></span></p>
+
+    <form>
+        <p style="text-align: center;">
+            <input type=button value="Refresh" onClick="window.location.reload()"></form>
+
+    <form action="statistics.php" method="get">
+
+        <p style="text-align: center;">
+            <span style="font-family:arial,helvetica,sans-serif;"><strong><em><span style="font-size: 24px;">Search another user: </span></em></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" /></p>
+    </form>
 
 </html>
--- stats.php
+++ stats.php
@@ -1,21 +1,94 @@
+<?php
+
+$link = mysql_connect ("203.33.121.41", "battles_stats", "")
+    or die('Could not connect: ' . mysql_error());
+
+mysql_select_db ("battle", $link) or die('could not select database');
+
+$totaldeaths = "SELECT SUM(deaths) as req_value FROM oresomebattles";
+$totaldeaths_results = mysql_query($totaldeaths)or die('query failed'. mysql_error());
+
+$row = mysql_fetch_assoc($totaldeaths_results);
+$final_deaths = $row['req_value'];
+
+$totalkills = "SELECT SUM(kills) as req_value FROM oresomebattles";
+$totalkills_results = mysql_query($totalkills)or die('query failed'. mysql_error());
+
+$row1 = mysql_fetch_assoc($totalkills_results);
+$final_kills = $row1['req_value'];
+mysql_close ($link);
+
+?>
+
 <html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-		<form action="statistics.php" method="get">
-			<p style="text-align: center;">
-				<strong><em><span style="font-size:48px;"><span style="font-family: arial, helvetica, sans-serif;">OresomeCraft Battles player statistics</span></span></em></strong></p>
-			<p style="text-align: center;">&nbsp;
-				</p>
-			<p style="text-align: center;">
-				<span style="font-family:arial,helvetica,sans-serif;"><strong><em><span style="font-size: 24px;">Username: </span></em></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" /></p>
-			<p style="text-align: center;">&nbsp;
-				</p>
-			<p style="text-align: center;">&nbsp;
-				</p>
-			<p style="text-align: center;">
-				<span style="font-family:arial,helvetica,sans-serif;"><span style="font-size: 22px;"><strong>Note:</strong> This website is in VERY early development and WILL be improved!</span></span></p>
-		</form>
-	</body>
+<head>
+    <title>OresomeCraft Battles online stats</title>
+</head>
+<style type="text/css">
+
+    .boxdiv {
+        margin: 5px;
+        padding: 5px;
+        width: 60%;
+        position: fixed;
+        left: 20%;
+        border: 2px solid #4D686F;
+        background-color: #E6E6E6;
+        border-radius: 8px;
+    }
+    .tdeathsdiv {
+        margin: 5px;
+        padding: 5px;
+        width: 45%;
+        position: fixed;
+        right: 2%;
+        border: 2px solid #4D686F;
+        background-color: #0000FF;
+        border-radius: 8px;
+    }
+    .tkillsdiv {
+        margin: 5px;
+        padding: 5px;
+        width: 45%;
+        position: fixed;
+        left: 2%;
+        border: 2px solid #4D686F;
+        background-color: #FF0000;
+        border-radius: 8px;
+    }
+</style>
+<body>
+
+<form action="statistics.php" method="get">
+    <p style="text-align: center;">
+        <strong><span style="font-size:48px;"><span style="font-family: arial, helvetica, sans-serif;">OresomeCraft Battles Statistics</span></span></strong></p>
+    <p style="text-align: center;">&nbsp;
+    </p>
+    <div class="boxdiv">
+        <p style="text-align: center;">
+            <strong><span style="font-size:32px;"><span style="font-family: arial, helvetica, sans-serif;">Search statistics for a user:</span></span></strong></p>
+        <p style="text-align: center;">&nbsp;
+            <span style="font-family:arial,helvetica,sans-serif;"><strong><span style="font-size: 20px;">Username: </span></strong></span><input name="name" type="text" /><span style="font-size:28px;"> </span><input type="submit" />
+        </p>
+    </div>
+</form>
+
+<br><br><br><br><br><br><br><br>
+<div class="tdeathsdiv">
+    <p style="text-align: center;">
+        <strong><span style="font-size:30px;"><span style="font-family: arial, helvetica, sans-serif;">Total Server Deaths:</span></span></strong></p>
+    <p style="text-align: center;">&nbsp;
+        <span style="font-family:arial,helvetica,sans-serif;"><strong><span style="font-size: 26px;"><?php echo $final_deaths ?></span></strong></span></p>
+
+</div>
+
+<div class="tkillsdiv">
+    <p style="text-align: center;">
+        <strong><span style="font-size:30px;"><span style="font-family: arial, helvetica, sans-serif;">Total Server Kills:</span></span></strong></p>
+    <p style="text-align: center;">&nbsp;
+        <span style="font-family:arial,helvetica,sans-serif;"><strong><span style="font-size: 26px;"><?php echo $final_kills ?></span></strong></span></p>
+
+</div>
+
+</body>
 </html>
